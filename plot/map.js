5c17e5495291f0b6f5c931e6303abe392f614290
Meggie Ladlow <m.ladlow@gmail.com>
2021-01-01 19:29:12 -0500

    Map working!

--- plot/map.js
+++ plot/map.js
@@ -1,6 +1,28 @@
 // © OpenStreetMap contributors
 // All map data and geocoding is available under the Open Database License
 // (https://www.openstreetmap.org/copyright).
+
+function handleMarkerClick(farm) {
+    let html = `
+        <h1>${farm["Name"]}</h1>
+        <p>${farm["Address"]}</p>
+    `;
+    let phone = farm["Phone"];
+    if (phone) {
+        html += `<p>${phone}</p>`;
+    }
+    let website = farm["Website"];
+    if (website) {
+        if (!website.startsWith("http")) {
+            website = `https://${website}`;
+        }
+        html += `<a target="_blank" href="${website}">${website}</a>`;
+    }
+
+    infoPane.innerHTML = html;
+}
+
+var infoPane = document.getElementById("info");
 var farmMap = L.map('map').setView([38.899, -77.084], 8);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -9,10 +31,13 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var markers = L.markerClusterGroup();
 
-for (var i = 0; i < farms.length; i++) {
-    var farm = farms[i];
-    var title = farm["Name"];
-    var marker = L.marker(new L.LatLng(farm["Lat"], farm["Lng"]), {title: title});
+for (let i = 0; i < farms.length; i++) {
+    let farm = farms[i];
+    let title = farm["Name"];
+    var marker = L.marker(new L.LatLng(farm["Lat"], farm["Lng"]), {title: title})
+        .on('click', function(e) {
+            handleMarkerClick(farm);
+        });
     marker.bindPopup(title);
     markers.addLayer(marker);
 }
